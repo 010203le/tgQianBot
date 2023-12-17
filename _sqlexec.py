0ad60f96c7447d277c0e5ca19b0e0f1e4afb2c2c
010203le <44265695+010203le@users.noreply.github.com>
2023-12-18 01:03:54 +0800

    Update _sqlexec.py

--- _sqlexec.py
+++ _sqlexec.py
@@ -1,3 +1,15 @@
 from _config import SQL_HOST, SQL_USER, SQL_PASS, SQL_DB
+import mysql.connector
+
+conn = mysql.connector.connect(
+    host='SQL_HOST', 
+    user='SQL_USER', 
+    password='SQL_PASS',
+)
+cursor = conn.cursor()
+cursor.execute(f"USE {SQL_DB};")
+
+
+conn.close()
 
 
