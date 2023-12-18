0faad9c346c92bf5dd1897392e8c20a6022a65b5
010203le <44265695+010203le@users.noreply.github.com>
2023-12-18 11:04:51 +0800

    Update _sqlexec_MS.py

--- _sqlexec_MS.py
+++ _sqlexec_MS.py
@@ -12,19 +12,19 @@ conn = pymssql.connect(host=SQL_HOST,
 
 def title_success(uid):
     with conn.cursor() as cursor:
-        update_query = "UPDATE tgQian SET score = score-50 WHERE id = "+str(uid)
+        update_query = f"UPDATE {SQL_DB} SET score = score-50 WHERE id = {uid}"
         cursor.execute(update_query)
         conn.commit()
 
 def title(uid):
     try:
         with conn.cursor() as cursor:
-            query = "SELECT * FROM tgQian WHERE id = " + str(uid)
+            query = f"SELECT * FROM {SQL_DB} WHERE id = {uid}"
             cursor.execute(query)
             existing_user = cursor.fetchone()
 
             if existing_user:
-                queryLast = "SELECT score FROM tgQian WHERE id = " + str(uid)
+                queryLast = f"SELECT score FROM {SQL_DB} WHERE id = {uid}"
                 cursor.execute(queryLast)
                 qScore = keepInt(cursor.fetchall())
                 if qScore < 50:
@@ -40,7 +40,7 @@ def title(uid):
 def lb():
     try:
         with conn.cursor() as cursor:
-            query = "SELECT TOP 5 [id], [times] FROM [tgQian].[dbo].[tgQian] ORDER BY [times] DESC;"
+            query = f"SELECT TOP 5 [id], [times] FROM [{SQL_DB}].[dbo].[tgQian] ORDER BY [times] DESC;"
             cursor.execute(query)
             result = cursor.fetchall()
             d1=''
@@ -54,12 +54,15 @@ def lb():
 def me(uid):
     try:
         with conn.cursor() as cursor:
-            query = "SELECT * FROM tgQian WHERE id = " + str(uid)
+            query = f"SELECT * FROM {SQL_DB} WHERE id = {uid}"
             cursor.execute(query)
             result = cursor.fetchall()
-            for item in result:
-                id_, time, score, passed = item
-            return f"""```
+            if result == []:
+                return ' 你好，查無此UID的簽到記錄。'
+            else:
+                for item in result:
+                    id_, time, score, passed = item
+                return f""" 的數據如下\n```
 上次簽到時間為 {datetime.datetime.fromtimestamp(time)}
 目前的積分為 {score}
 累積簽到次數為 {passed}
@@ -74,28 +77,28 @@ def q(uid, luck):
     last = int(time.time())
     try:
         with conn.cursor() as cursor:
-            query = "SELECT * FROM tgQian WHERE id = " + str(uid)
+            query = f"SELECT * FROM {SQL_DB} WHERE id = {uid}"
             cursor.execute(query)
             existing_user = cursor.fetchone()
 
             if existing_user:
-                queryLast = "SELECT last FROM tgQian WHERE id = " + str(uid)
+                queryLast = f"SELECT last FROM {SQL_DB} WHERE id = {uid}"
                 cursor.execute(queryLast)
                 qLast = keepInt(cursor.fetchall())
                 rq = last-qLast
                 if rq < 86400:
                     return('你已經簽到過了，請於 '+str(int((86400-(rq))/3600))+' 小時 '+str(int(((86400-(rq))%3600)/60))+' 分後重試。')
                 else:
-                    update_query = "UPDATE tgQian SET times = times + 1, score = score+"+str(luck)+", last = "+str(last)+" WHERE id = "+str(uid)
+                    update_query = f"UPDATE {SQL_DB} SET times = times + 1, score = score+{luck}, last = {last} WHERE id = {uid}"
                     cursor.execute(update_query)
                     conn.commit()
                     cursor.execute('SELECT times from tgQian WHERE id = '+str(uid))
-                    return("這是你第 "+str(keepInt(cursor.fetchall()))+"次簽到，本次簽到獲得 "+str(luck)+" 分")
+                    return("這是你第 "+str(keepInt(cursor.fetchall()))+f"次簽到，本次簽到獲得 {luck} 分")
             else:
-                insert_query = "INSERT INTO tgQian Values("+str(uid)+","+str(last)+","+str(luck)+",1)"
+                insert_query = f"INSERT INTO tgQian Values({uid},{last},{luck},1)"
                 cursor.execute(insert_query)
                 conn.commit()
-                return('這是你第 1 次簽到，本次簽到獲得 '+str(luck)+' 分')
+                return(f'這是你第 1 次簽到，本次簽到獲得 {luck} 分')
     except Exception as e:
         print(f"Error: {e}")
         return('數據庫發生錯誤')
